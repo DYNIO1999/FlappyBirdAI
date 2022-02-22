55070efb4bd051f7a32c26d1310f40f78d46a3ed
Mariusz Połetek <s090169@student.tu.kielce.pl>
2022-02-22 18:50:15 +0100

    VS Project Setup - Test Window
    Just basic test window. GLAD putted into source. Include paths in GLAD sources was changed from the original <> to "" .

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,57 @@
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
 #include <iostream>
-#include "Engine.h"
 
-int main(int argc, char const *argv[])
-{   
+
+
+bool initGLAD()
+{
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize OpenGL context" << std::endl;
+        return false;
+    }
+    return true;
+}
+
+
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(800, 600, "Flappy Bird AI", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+
+    initGLAD();
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
